776af9d2b87e97f2eed8fae8ec971b26763cb4f9
Shubham Aher <ahers247@gmail.com>
2022-02-15 21:43:48 +0530

    these

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,36 @@
-import React, { Component, useState } from "react";
+import React, { Component, useEffect, useState } from "react";
 import '../styles/App.css';
+import React from 'react'
 
-class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            renderBall: false,
-            posi : 0,
-            ballPosition: { left: "0px" }
-        };
-        this.renderChoice = this.renderBallOrButton.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
-    };
-
-    buttonClickHandler() {
+export const App = () => {
+    const[renderBall,setRenderBall]=useState(false);
+    const[position , setPosition]=useState(0);
+    const[ballPosition,setBallPosition]=useState({left:"0px"});
+    function buttonClickHandler() {
+        setRenderBall(true)
    
-   }
-    renderBallOrButton() {
-		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
+    }
+  
+    function  renderBallOrButton() {
+		if (renderBall) {
+		    return <div className="ball" style={{position:"absolute",left:ballPosition.left}}></div>
 		} else {
-		    return <button onClick={this.buttonClickHandler} >Click For One Ball</button>
+		    return <button onClick={buttonClickHandler} >Click For One Ball</button>
 		}
     }
+    useEffect(()=>{
+        document.addEventListener("keydown",(e)=>{
+           if(e.key==="ArrowRight"){
+               setBallPosition({left:ballPosition.left+5})
+           }
+        })
 
-    // bind ArrowRight keydown event
-    componentDidMount() {
-      
-    }
-
-    render() {
-        return (
-            <div className="playground">
-                {this.renderBallOrButton()}
+    },[])
+  return (
+     <div className="playground">
+                {renderBallOrButton()}
             </div>
-        )
-    }
+  )
 }
 
 
