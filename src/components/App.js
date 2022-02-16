574d8accaabec1f8b6e0fadc44e1118c9e10233b
Shubham  Aher <84302530+Shubhamaher1@users.noreply.github.com>
2022-02-16 09:32:59 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,35 @@
 import React, { Component, useEffect, useState } from "react";
-import '../styles/App.css';
+import "../styles/App.css";
 // import React from 'react'
 
-export const App = () => {
-    const[renderBall,setRenderBall]=useState(false);
-    const[position , setPosition]=useState(0);
-    const[ballPosition,setBallPosition]=useState({left:"0px"});
-    function buttonClickHandler() {
-        setRenderBall(true);
-   
-    }
-  function updateBall(event){
-    if(event.key==="ArrowRight"){
-        setBallPosition(prev=>({left:prev.left+5}))
+function App() {
+  const [renderBall, setRenderBall] = useState(false);
+  const [position, setPosition] = useState(0);
+  const [ballPosition, setBallPosition] = useState({ left: 0 });
+  function buttonClickHandler() {
+    setRenderBall(true);
+  }
+  function updateBall(event) {
+    if (event.key === "ArrowRight") {
+      setBallPosition((prev) => ({ left: prev.left + 5 }));
     }
   }
-    function  renderBallOrButton() {
-		if (renderBall) {
-		    return <div className="ball" style={{position:"absolute",left:ballPosition.left}}></div>
-		} else {
-		    return <button onClick={buttonClickHandler} >Click For One Ball</button>
-		}
+  function renderBallOrButton() {
+    if (renderBall) {
+      return (
+        <div
+          className="ball"
+          style={{ position: "absolute", left: ballPosition.left }}
+        ></div>
+      );
+    } else {
+      return <button onClick={buttonClickHandler}>Click For One Ball</button>;
     }
-    useEffect(()=>{
-        document.addEventListener("keydown",updateBall)
-
-    },[])
-  return (
-     <div className="playground">
-                {renderBallOrButton()}
-            </div>
-  )
+  }
+  useEffect(() => {
+    document.addEventListener("keydown", updateBall);
+  }, []);
+  return <div className="playground">{renderBallOrButton()}</div>;
 }
 
-
-// export default App;
+export default App;
